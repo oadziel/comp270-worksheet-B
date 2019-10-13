a95e322bd117c63d8689e24a2250dd390f89f6fe
edpowley <edpowley@gmail.com>
2019-10-13 18:54:46 +0100

    Added wind and shot angle calculation

--- Controller.cpp
+++ Controller.cpp
@@ -3,6 +3,13 @@
 
 float Controller::calculateShotSpeed(const Vector2& tankPos, const Vector2& enemyPos, float shotAngleRadians, float gravity)
 {
-	// TODO
+	// TODO: calculate the required shot speed (in pixels per second) and return it
 	return 700;
 }
+
+float Controller::calculateShotAngle(const Vector2& tankPos, const Vector2& enemyPos, float shotSpeed, float gravity)
+{
+	// TODO: calculate the required shot angle (in radians) and return it
+	return M_PI * 0.25f;
+}
+
--- Controller.h
+++ Controller.h
@@ -6,7 +6,9 @@ class Controller
 {
 public:
 	float calculateShotSpeed(const Vector2& tankPos, const Vector2& enemyPos, float shotAngleRadians, float gravity);
+	float calculateShotAngle(const Vector2& tankPos, const Vector2& enemyPos, float shotSpeed, float gravity);
 
 	const float c_canHandleHeightDifference = false;
-
+	const float c_canHandleWind = false;
+	const float c_doCalculateAngle = false;
 };
