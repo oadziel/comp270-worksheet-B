ae0d38daff20c2d6ee021855e151b5709a0f44b9
oadziel <josh@jlewisgames.co.uk>
2020-03-16 01:02:49 +0000

    Implemented formula for calculating shot speed required to hit the enemy tank - Task 1 Complete

--- Controller.cpp
+++ Controller.cpp
@@ -8,9 +8,18 @@
 //   gravity: the acceleration due to gravity in pixels/second^2 (positive is downwards)
 //   wind: the acceleration due to wind in pixels/second^2 (positive is rightwards)
 float Controller::calculateShotSpeed(const Vector2& tankPos, const Vector2& enemyPos, float shotAngleRadians, float gravity, float wind)
-{
-	// TODO: calculate the required shot speed (in pixels per second) and return it
-	return 700;
+{	
+	// formula u = root(x * g / sin2(theta))
+
+	float distancex = enemyPos.x - tankPos.x; //distance x between the tank and enemy
+
+	float thtSin = sin(shotAngleRadians); //sin of shot angle
+	float thtCos = cos(shotAngleRadians); //cos of shot angle
+	float thtSin2 = 2 * thtSin * thtCos; //Sin2Theta of the shot angle
+    //Calculate required speed by the formula Speed = Sqrt(Distance x * gravity / Sin2Theta of shot angle)
+	float sSpeed = sqrt(distancex * gravity / thtSin2); 
+
+	return sSpeed;
 }
 
 // Calculate the shot angle to hit the target, given the following information.
